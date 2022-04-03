6a3649b8499880be661ed2061220465a144943a3
Sword <75vbb11jn@mozmail.com>
2022-04-03 20:17:14 +0300

    Started the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Profile-modified.png';
 import './App.css';
+
+import discord from './discord.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Hello there!</p>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Im a Flutter & Full-Stack Developer.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+
       </header>
     </div>
   );
