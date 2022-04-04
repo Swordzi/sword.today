c34e5914225de1f178d67838a098bab6b473179e
Sword <75vbb11jn@mozmail.com>
2022-04-04 19:14:03 +0300

    Started working on background video

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,29 @@ import React from 'react';
 import logo from './Profile-modified.png';
 import './App.css';
 
-import discord from './discord.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello there!</p>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Im a Flutter & Full-Stack Developer. (More coming soon)
-        </p>
 
 
 
-      </header>
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <video autoPlay loop muted id='video'>
+                    <source
+                        src="https://www.youtube.com/watch?v=HKT9wctwL6g"
+                        type='video/mp4'/>
+                </video>
+
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Sword
+                </p>
+
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
