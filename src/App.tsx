bbd312730f9ecdddb12f8c20e0e09f817b001616
Sword <75vbb11jn@mozmail.com>
2022-04-08 17:08:28 +0300

    Working on background video is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
+import React from "react";
 import logo from './Profile-modified.png';
 import './App.css';
+import backgroundVideo from './videos/bgvideo.mp4'
 
-
-
-
-
-function App() {
+function frontPage() {
     return (
         <div className="App">
             <header className="App-header">
-                <video autoPlay loop muted id='video'>
-                    <source
-                        src="https://www.youtube.com/watch?v=HKT9wctwL6g"
-                        type='video/mp4'/>
+                <video autoPlay loop muted id='background'>
+                    <source src={backgroundVideo} type='video/mp4'/>
                 </video>
 
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Sword
-                </p>
 
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p id="Name">Sword</p>
 
             </header>
         </div>
+
     );
 }
 
-export default App;
+export default frontPage;
