c2a844a7ad1213ed63159cf2120d1a9942613419
Sword <75vbb11jn@mozmail.com>
2022-04-13 21:42:33 +0300

    Changed background video

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from './Profile-modified.png';
 import './App.css';
+import waves from './videos/waves.mp4'
 
-function App() {
+function frontPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <video autoPlay loop muted id='background'>
+            <source src={waves} type='video/mp4'/>
+          </video>
+
+
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p id="Name">Sword</p>
+
+        </header>
+      </div>
+
   );
 }
 
-export default App;
+export default frontPage;
